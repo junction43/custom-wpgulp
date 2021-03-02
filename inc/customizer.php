8d0036261e79cd75f55cb1b466712d3c7b3929d9
Nina Pang <niina.pang@gmail.com>
2021-03-02 13:28:06 +0100

    Upload starter theme from Underscores

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * custom-wpgulp Theme Customizer
+ *
+ * @package custom-wpgulp
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function custom_wpgulp_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'custom_wpgulp_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'custom_wpgulp_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'custom_wpgulp_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function custom_wpgulp_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function custom_wpgulp_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function custom_wpgulp_customize_preview_js() {
+	wp_enqueue_script( 'custom-wpgulp-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'custom_wpgulp_customize_preview_js' );
